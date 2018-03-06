a5195417f6f099a5189d110db37a82794ced530e
Yue Huang <yuehuang@Yues-MacBook-Pro.local>
2018-03-06 10:42:42 -0500

    Initial Commit

---
+++ simulator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  simulator
+//
+//  Created by Yue Huang on 2018-03-06.
+//  Copyright © 2018 Yue Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
